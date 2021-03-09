974200a16d90b1de2d1186a6b8ba64f652a4d2df
AAAAHHHHHH <71350188+AAAAHHHHHH@users.noreply.github.com>
2021-03-09 09:53:50 -0600

    Version 0.1

--- random_picker/gamestuff.js
+++ random_picker/gamestuff.js
@@ -1,4 +1,6 @@
 //Define player choices and choose a random one of them.
-let pickRandom = (playerChoice) = {
-    
+let pickRandom = (playerChoice = []) => {
+    let randomNumber = Math.floor(Math.random() * playerChoice.length)
+    let chosenOne = playerChoice[randomNumber]
+    return chosenOne
 }
