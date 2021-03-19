5fe864172b2eca96edb42f19a0ec27ff0c148551
AAAAHHHHHH <71350188+AAAAHHHHHH@users.noreply.github.com>
2021-03-19 12:57:42 -0500

    Begin testing for the "Null Array" Error.

--- random_picker/gamestuff.js
+++ random_picker/gamestuff.js
@@ -1,6 +1,15 @@
 //Define player choices and choose a random one of them.
-let pickRandom = (playerChoice = []) => {
-    let randomNumber = Math.floor(Math.random() * playerChoice.length)
-    let chosenOne = playerChoice[randomNumber]
-    return chosenOne
+let RANDOM_WORD = ''
+let PLAYER_INPUTS = []
+let updateInputs = () => {
+    PLAYER_INPUTS.push(playerInput)
+}
+let pickRandom = () => {
+    let randomNumber = Math.floor(Math.random() * PLAYER_INPUTS.length)
+    RANDOM_WORD = PLAYER_INPUTS[randomNumber]
+    return RANDOM_WORD
+}
+let showWork = () => {
+    let randWord = document.getElementById('randobj')
+    randWord.innerHTML = RANDOM_WORD
 }
