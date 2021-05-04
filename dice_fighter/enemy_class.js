c7ca36107f60fa6d74d67aa4dfdafb663dc6857e
AAAAHHHHHH <rydia.a.titus@gmail.com>
2021-05-04 10:52:03 -0500

    Added an Enemy class and some Player stats

---
+++ dice_fighter/enemy_class.js
@@ -0,0 +1,11 @@
+class Enemy {
+    constructor(name,health, defense, damage, aspect, EXP, gold) {
+        this.heath = health
+        this.defense = defense
+        this.damage = damage
+        this.aspect = aspect
+        this.name = name
+        this.EXP = EXP
+        this.gold = gold
+    }
+}
--- dice_fighter/main.js
+++ dice_fighter/main.js
@@ -1,2 +1,12 @@
-const Basic_Attack = new Ability('Basic Attack', 100, 75, 'Normal',
+//Enemy/Ability Definitions
+let Basic_Attack = new Ability('Basic Attack', 20, 75, 'Normal',
                         'Deliver a basic attack.', 'Attack')
+let Goblin = new Enemy('Goblin', 50, 2, 1, 'Normal', 5, 5)
+console.log(Goblin)
+//Player Stats
+let hp = 10
+let attack = 1
+let defense = 3
+let wealth = 0
+let exp = 0
+//Battle System?
